9bcfdecd6b733d67c659906f1575a690282fbd1b
dmitriys <dmitriys@codevalue.net>
2017-08-15 16:33:52 +0300

    added ICopyable

---
+++ SharpToolkit.Extensions/ICopyable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpToolkit.Extensions
+{
+    /// <summary>
+    /// Marks class as being able to safely copy itself.
+    /// </summary>
+    /// <typeparam name="T">The type of the class.</typeparam>
+    public interface ICopyable<T>
+    {
+        /// <summary>
+        /// Safely copies the object.
+        /// </summary>
+        /// <returns></returns>
+        T Copy();
+    }
+}
