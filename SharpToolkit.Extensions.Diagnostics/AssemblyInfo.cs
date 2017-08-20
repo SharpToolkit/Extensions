40657f556525b8fac8170b5bd656195adbdf8f04
dmitriys <dmitriys@codevalue.net>
2017-08-20 16:53:25 +0300

    Renamed Extensions.Testing to Extensions.Diagnostics

--- SharpToolkit.Extensions.Diagnostics/AssemblyInfo.cs
+++ SharpToolkit.Extensions.Diagnostics/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-[assembly: InternalsVisibleTo("SharpToolkit.Extensions.Testing.Test")]
+[assembly: InternalsVisibleTo("SharpToolkit.Extensions.Diagnostics.Test")]
