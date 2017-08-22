f851b7f6d71064a8e0450e334c6ea87ae63f4401
dmitriys <dmitriys@codevalue.net>
2017-08-22 19:08:42 +0300

    Added identity function to linq

---
+++ SharpToolkit.Extensions.Collections/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpToolkit.Extensions.Collections
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Identity function for debug purposes. The collection will be passed to the supplied action
+        /// so it could be observed.
+        /// </summary>
+        /// <typeparam name="T">Collection type.</typeparam>
+        /// <param name="e">The collection.</param>
+        /// <param name="action">Action to which the collection will be passed.</param>
+        /// <returns>Unchanged collection.</returns>
+        public static IEnumerable<T> Identity<T>(this IEnumerable<T> e, Action<IEnumerable<T>> action)
+        {
+            action(e);
+            return e;
+        }
+    }
+}
