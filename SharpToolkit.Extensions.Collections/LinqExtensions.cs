6d2271c55e8cf8a6cf74d885f9ca999d103dd092
dmitriys <dmitriys@codevalue.net>
2017-08-23 11:03:35 +0300

    Changed identity function signature

--- SharpToolkit.Extensions.Collections/LinqExtensions.cs
+++ SharpToolkit.Extensions.Collections/LinqExtensions.cs
@@ -7,16 +7,16 @@ namespace SharpToolkit.Extensions.Collections
     public static class LinqExtensions
     {
         /// <summary>
-        /// Identity function for debug purposes. The collection will be passed to the supplied action
+        /// Identity function for debug purposes. The collection will be passed to the supplied function
         /// so it could be observed.
         /// </summary>
         /// <typeparam name="T">Collection type.</typeparam>
         /// <param name="e">The collection.</param>
-        /// <param name="action">Action to which the collection will be passed.</param>
+        /// <param name="fn">The function to which the collection will be passed.</param>
         /// <returns>Unchanged collection.</returns>
-        public static IEnumerable<T> Identity<T>(this IEnumerable<T> e, Action<IEnumerable<T>> action)
+        public static IEnumerable<T> Identity<T>(this IEnumerable<T> e, Func<IEnumerable<T>, IEnumerable<T>> fn)
         {
-            action(e);
+            fn(e);
             return e;
         }
     }
