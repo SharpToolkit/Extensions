7742c441a1c9219b18f627b470400f05acf52f39
dmitriys <dmitriys@codevalue.net>
2017-09-07 13:27:02 +0300

    Added TrimTo to string extensions

---
+++ SharpToolkit.Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpToolkit.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimTo(this string str, int length)
+        {
+            length = length > str.Length ? str.Length : length;
+
+            return str.Substring(0, length);
+        }
+    }
+}
